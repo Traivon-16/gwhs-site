28630395b7bca7c1f87d9d405905c3ede999e37a
Traivon-16 <Twhit0121@withs.org>
2015-03-09 09:09:12 -0400

    Create guts.js

---
+++ calculator-project/js/guts.js
@@ -0,0 +1,34 @@
+
+function divide(x, y)
+{
+	return (x/y);
+}
+
+function multiply(x, y)
+{
+    return x*y;
+}
+function add(x, y)
+{
+    return x+y
+}
+function power(x, y)
+{
+	return x^y;
+}
+function square_root(x)
+{
+	return Math.sqrt(x);
+}
+function logbase10(x)
+{
+	return Math.log10(x);
+}
+function subtract(x, y)
+{
+   return (x-y);
+}
+function generateRandomNumber()
+{
+	return Math.floor(Math.random() * (999 - 0 + 1)) + 1;
+}
